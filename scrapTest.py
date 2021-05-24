73484b46fdac629be6a0f1d43d678d5134f4a5e7
AlphonseBrandon <alphonse.brand@gmail.com>
2021-05-24 11:55:28 +0100

    using the urllib3 and beautiful soup to extract and print the title of a web page

--- scrapTest.py
+++ scrapTest.py
@@ -2,6 +2,6 @@ import urllib3
 from bs4 import BeautifulSoup
 http = urllib3.PoolManager()
 r = http.request('GET', 'https://authoraditiagarwal.com')
-soup = BeautifulSoup(r.data, 'lxml')
+soup = BeautifulSoup(r.data, 'lxml') #extraction
 print (soup.title)
-print (soup.title.text)
+print (soup.title.text) #printing out title
